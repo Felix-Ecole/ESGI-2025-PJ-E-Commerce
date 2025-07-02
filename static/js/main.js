50fd47be3de168d837b7a7a5aa296078ea1377cc
Felix-Ecole <124139583+Felix-Ecole@users.noreply.github.com>
2025-07-03 01:22:21 +0200

    Revert "Merge Feature/javascript into master"

--- static/js/main.js
+++
@@ -1,8 +0,0 @@
-// Fonction utilitaire qui retourne la data des jeu de donnée.
-export async function importData(filename) {
-	let path = "static/data/" + filename
-
-	const r = await fetch(path);
-	if (r.ok) return await r.json()
-	throw new Error(`fail to fetch ${path}`)
-}
